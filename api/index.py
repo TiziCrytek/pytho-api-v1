297906db319c173b9e62aad598bea4f609c96b24
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2022-12-29 17:59:14 +0200

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,25 +1,26 @@
 from flask import Flask
+from tradingview_ta import TA_Handler, Interval
 
 app = Flask(__name__)
-
-# bank = TA_Handler(
-#     symbol = 'EURUSD',
-#     exchange = "FX_IDC", 
-#     screener = "forex", 
-#     interval = Interval.INTERVAL_1_MINUTE, 
-#     timeout=None,
-# )
-# res = {
-#     'summary': bank.get_analysis().summary["RECOMMENDATION"],
-#     'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
-#     'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
-# }
 
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/get')
+def get():
+    bank = TA_Handler(
+        symbol = 'EURUSD',
+        exchange = "FX_IDC", 
+        screener = "forex", 
+        interval = Interval.INTERVAL_1_MINUTE, 
+        timeout=None,
+    )
+    res = {
+        'summary': bank.get_analysis().summary["RECOMMENDATION"],
+        'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
+        'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
+    }
+
+    return res
