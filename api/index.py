3c545078b30c3466776435ebc635a6a3cc58f58e
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2023-10-20 18:51:23 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from tradingview_ta import TA_Handler, Interval
+from server import Server
 
 app = Flask(__name__)
 CORS(app)
@@ -30,3 +31,4 @@ def get():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    server = Server()
