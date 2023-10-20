4ebc4d816b204e0b1d4616b34ca61e6d9d7321c2
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2023-10-20 21:00:21 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,50 +2,63 @@ from flask import Flask, request
 from flask_cors import CORS
 from tradingview_ta import TA_Handler, Interval
 from os.path import join
-
 import json
 
-app = Flask(__name__)
-CORS(app)
-cors = CORS(app, origins = '*')
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        cors = CORS(self.app, origins = '*')
 
-@app.route('/', methods=['POST'])
-def test():
-    return ok, 500
+        self.connect = True
 
-@app.route('/test', methods=['POST'])
-def handle_post_request():
-    if request.method == 'POST':
-        data = request.json  # Получаем данные POST-запроса в формате JSON
-        # Обрабатываем данные...
-        return data # Отправляем ответ на POST-запрос
-    else:
-        return 'Invalid request method'
+        @self.app.route('/', methods=['POST'])
+        def root():
+            if self.connect:
+                return '', 200
+            else:
+                return '', 404
 
-@app.route('/gg')
-def gg():
-    keys = 'NO NO NO'
-    with open(join('data', 'keys.json'), 'r') as file:
-        keys = json.load(file)
-        file.close()
-    return keys
+        @self.app.route('/system', methods=['POST'])
+        def handle_post_request():
+            if request.method == 'POST':
+                data = request.json  # Получаем данные POST-запроса в формате JSON
+                # Обрабатываем данные...
+                return data # Отправляем ответ на POST-запрос
+            else:
+                return 'Invalid request method'
 
-@app.route('/get')
-def get():
-    bank = TA_Handler(
-        symbol = 'EURUSD',
-        exchange = "FX_IDC", 
-        screener = "forex", 
-        interval = Interval.INTERVAL_1_MINUTE, 
-        timeout=None,
-    )
-    res = {
-        'summary': bank.get_analysis().summary["RECOMMENDATION"],
-        'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
-        'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
-    }
+        @self.app.route('/login', methods=['POST'])
+        def login():
+            pass
 
-    return res
+        @self.app.route('/gg')
+        def gg():
+            keys = 'NO NO NO'
+            with open(join('data', 'keys.json'), 'r') as file:
+                keys = json.load(file)
+                file.close()
+            return keys
+
+        @self.app.route('/get')
+        def get():
+            bank = TA_Handler(
+                symbol = 'EURUSD',
+                exchange = "FX_IDC", 
+                screener = "forex", 
+                interval = Interval.INTERVAL_1_MINUTE, 
+                timeout=None,
+            )
+            res = {
+                'summary': bank.get_analysis().summary["RECOMMENDATION"],
+                'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
+                'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
+            }
+
+            return res
+
+    def run(self):
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = Server()
+    app.run()
