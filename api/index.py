b6116505b8309050603a38b66d0744d0fe7a09ee
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2023-10-20 18:29:32 +0300

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_cors import CORS
+from tradingview_ta import TA_Handler, Interval
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, origins = '*')
+
+try:
+    @app.route('/')
+    def home():
+        return 'Hello, World!'
+    
+    @app.route('/get')
+    
+    def get():
+        bank = TA_Handler(
+            symbol = 'EURUSD',
+            exchange = "FX_IDC", 
+            screener = "forex", 
+            interval = Interval.INTERVAL_1_MINUTE, 
+            timeout=None,
+        )
+        res = {
+            'summary': bank.get_analysis().summary["RECOMMENDATION"],
+            'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
+            'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
+        }
+    
+        return res
+except Exception as e:
+    print(e)
