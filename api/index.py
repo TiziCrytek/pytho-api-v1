28017c3fc941eafffd4ce25780eb677f59f504ca
TiziCrytek <121620908+TiziCrytek@users.noreply.github.com>
2023-10-20 18:17:23 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,24 +6,27 @@ app = Flask(__name__)
 CORS(app)
 cors = CORS(app, origins = '*')
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/get')
-
-def get():
-    bank = TA_Handler(
-        symbol = 'EURUSD',
-        exchange = "FX_IDC", 
-        screener = "forex", 
-        interval = Interval.INTERVAL_1_MINUTE, 
-        timeout=None,
-    )
-    res = {
-        'summary': bank.get_analysis().summary["RECOMMENDATION"],
-        'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
-        'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
-    }
-
-    return res
+try:
+    @app.route('/')
+    def home():
+        return 'Hello, World!'
+    
+    @app.route('/get')
+    
+    def get():
+        bank = TA_Handler(
+            symbol = 'EURUSD',
+            exchange = "FX_IDC", 
+            screener = "forex", 
+            interval = Interval.INTERVAL_1_MINUTE, 
+            timeout=None,
+        )
+        res = {
+            'summary': bank.get_analysis().summary["RECOMMENDATION"],
+            'oscillators': bank.get_analysis().oscillators["RECOMMENDATION"],
+            'moving_averages': bank.get_analysis().moving_averages["RECOMMENDATION"]
+        }
+    
+        return res
+except Exception as e:
+    print(e)
